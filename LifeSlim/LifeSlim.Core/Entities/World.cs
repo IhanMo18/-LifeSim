09fd1995998b2ca0eebc7b9dde782fd15298db33
IhanMo18 <ihanmontalvan@gmail.com>
2025-04-24 23:35:07 -0400

    fix(core)-->create a constructor for {World.cs} whit Width and Height and Modify List<Races> for List<Creature>

--- LifeSlim/LifeSlim.Core/Entities/World.cs
+++ LifeSlim/LifeSlim.Core/Entities/World.cs
@@ -4,9 +4,23 @@ namespace LifeSlim.Core.Entities;
 
 public class World
 {
+    public World(int width, int height)
+    {
+        Width = width;
+        Height = height;
+        grid = new int[width, height];
+    }
     
-    public List<Race> Races { get; set; } = new();
+    public int Width { get; }
+    public int Height { get; }
+    
+    private int[,] grid;
+    
+    public List<Creature> Creatures { get; set; } = new();
     public int YearTime { get; set; }
     public List<WorldEvent> ScheduledEvents { get; set; } = new();
+    
+    // Dictionary<(int, int), List<GameObject>> worldMap = new Dictionary<(int, int), List<GameObject>>();
+    
     
 }
