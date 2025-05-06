56b3b229fadf261f06c020e0b45966d4daf80263
IhanMo18 <ihanmontalvan@gmail.com>
2025-05-06 10:11:55 -0400

    feat(core)--->Updated with method MoveCreaturesInWorld by moving creatures in the World

--- LifeSlim/LifeSlim.Core/System/MovementSystem.cs
+++ LifeSlim/LifeSlim.Core/System/MovementSystem.cs
@@ -1,29 +1,34 @@
 using LifeSlim.Core.Model;
-using LifeSlim.Core.Movement;
-
-namespace LifeSlim.Core.System;
 
 public class MovementSystem(MovementStrategyFactory strategyFactory)
 {
-    public void Move(World world, Creature creature)
+    public async Task Move(World world, Creature creature)
     {
         var currentKey = $"{creature.Position.X},{creature.Position.Y}";
         world.CreaturePositions.Remove(currentKey); // Elimina la posición actual
 
         var strategy = strategyFactory.GetStrategy(creature);
-        var nextPosition = strategy.NextPosition(world, creature);
+        var nextPosition = await strategy.NextPosition(world, creature);
         var newKey = $"{nextPosition.X},{nextPosition.Y}";
 
         while (world.CreaturePositions.ContainsKey(newKey))
         {
             strategy = strategyFactory.GetStrategy(creature);
-            nextPosition = strategy.NextPosition(world, creature);
+            nextPosition = await strategy.NextPosition(world, creature);
             newKey = $"{nextPosition.X},{nextPosition.Y}";
         }
-        
+
         creature.Position = nextPosition;
         world.CreaturePositions[newKey] = creature.Id.ToString();
-        
     }
 
+
+    public async void MoveCreaturesInWorld(World world)
+    {
+        foreach (var creature in world.Creatures)
+        {
+            await Move(world, creature);
+            creature.AgeOneYear();                      
+        }
+    }
 }
