9a933f0106183414c02da7a06eb8d537a60cb09b
David Orrego <orregod091@gmail.com>
2025-05-07 17:56:53 -0400

    {Add} inject FoodFactorie.cs {Refactor} world.MapObjects

--- LifeSlim/LifeSlim.Core/System/ObjectsSystem.cs
+++ LifeSlim/LifeSlim.Core/System/ObjectsSystem.cs
@@ -9,17 +9,19 @@ public class ObjectsSystem
 {
     public readonly World _world;
     public readonly ICreatureFactory _creatureFactory;
+    private readonly IFoodFactorie _foodFactorie;
 
-    public ObjectsSystem(World world, ICreatureFactory creatureFactory)
+    public ObjectsSystem(World world, ICreatureFactory creatureFactory, IFoodFactorie foodFactorie)
     {
         _world = world;
         _creatureFactory = creatureFactory;
+        _foodFactorie = foodFactorie;
     }
 
 
     public void AddCreatures(int amount)
     {
-        while (_world.Creatures.Count < amount)
+        while (_world.MapObjects.OfType<Creature>().Count() < amount)
         {
             try
             {
@@ -29,9 +31,9 @@ public class ObjectsSystem
                     .Build();
 
                 var creature = _creatureFactory.CreateCreature(_world, race);
-                _world.Creatures.Add(creature);
+                _world.MapObjects.Add(creature);
                 _world.CreaturePositions.Add($"{creature.Position.X},{creature.Position.Y}", creature.Id.ToString());
-                Console.WriteLine("EL COUNT ES " + _world.Creatures.Count);
+                Console.WriteLine("EL COUNT De Criatura ES " + _world.MapObjects.OfType<Creature>().Count());
             }
             catch (Exception ex)
             {
@@ -39,11 +41,33 @@ public class ObjectsSystem
             }
         }
 
+        while (_world.MapObjects.OfType<Food>().Count() < _world.MapObjects.OfType<Creature>().Count() * 2)
+        {
+            var food = _foodFactorie.CreateFood();
+            _world.MapObjects.Add(food);
+            _world.CreaturePositions.Add($"{food.Position.X},{food.Position.Y}", food.Id.ToString());
+            Console.WriteLine("EL COUNT de Food ES " + _world.MapObjects.OfType<Food>().Count());
+        }
+
         for (var i = 0; i < _world.Width; i++)
         {
             for (var j = 0; j < _world.Height; j++)
             {
-                Console.Write(_world.CreaturePositions.ContainsKey($"{i},{j}") ? "*" : "-");
+                if (_world.CreaturePositions.TryGetValue($"{i},{j}", out var mapObjectId))
+                {
+                    var mapObject = _world.MapObjects.FirstOrDefault(mo =>
+                        string.Equals($"{mo.Id}", mapObjectId));
+                    
+                    if (mapObject?.GetType().Name == "Food")
+                    {
+                        Console.Write("#");    
+                    }
+                    else
+                    {
+                        Console.Write("*");    
+                    }
+                }
+                Console.Write("-");    
             }
 
             Console.WriteLine();
@@ -52,7 +76,7 @@ public class ObjectsSystem
 
     public void RemoveCreatures()
     {
-        _world.Creatures.RemoveAll(c => c.IsAlive==false);
+        _world.MapObjects.OfType<Creature>().ToList().RemoveAll(c => c.IsAlive==false);
     }
 
 }
